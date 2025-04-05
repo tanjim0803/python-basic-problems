04f74ee2f89585d11c9066491f8074078e54b6f3
Tanjim Ahmed <tanjimahmed1905@gmail.com>
2025-04-05 20:23:25 +0600

    added problem 06

---
+++ 06_problem.py
@@ -0,0 +1,9 @@
+# Write a Python program to convert kilometers to miles.
+
+kimometers = float(input(f"Enter the distance in KM: "))
+
+one_km =  0.621371
+
+miles = one_km * kimometers
+
+print(f"{kimometers} is equal to {miles} miles.")
